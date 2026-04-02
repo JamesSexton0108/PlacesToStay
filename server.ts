cd2d3b3539ae83ea3ef47c12315da3c5f51c6db0
James Sexton <0sextj12@solent.ac.uk>
2026-04-02 10:39:32 +0100

    Added functionality for inserting new bookings into the acc_bookings table.

--- server.ts
+++ server.ts
@@ -6,6 +6,9 @@ const app = express();
 const PORT = 3000;
 const db = new Database("placestostay.db")
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.get('/accommodation/location/:location', (req, res) => {
 	const stmt = db.prepare("SELECT * FROM accommodation WHERE location=? COLLATE NOCASE");
 	const results = stmt.all(req.params.location);
@@ -18,6 +21,14 @@ app.get('/accommodation/type/:type/location/:location', (req, res) => {
 	);
 	const results = stmt.all(req.params.type, req.params.location);
 	res.json(results);
+});
+
+app.post('/booking', (req, res) => {
+
+	const insertstmt = db.prepare('INSERT INTO acc_bookings(accID, thedate, userID, npeople) VALUES(?,?,?,?)');
+	const info = insertstmt.run(req.body.accID, req.body.thedate, req.body.userID, req.body.npeople);
+	res.json({id: info.lastInsertRowid})
+
 });
 
 
